59f15c0f50b5d541409c9a3e0bd1925a75e1d4d7
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2024-09-26 15:14:40 +0000

    feat: added compute resources

---
+++ compute.tf
@@ -0,0 +1,24 @@
+resource "aws_key_pair" "ec2_auth" {
+  key_name   = "my_ssh_key_pair"
+  public_key = file("./my-ssh-key-pair.pub")
+
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.public_ec2_ami.id
+
+  tags = {
+    Name = "dev_node"
+
+  }
+
+  key_name               = aws_key_pair.ec2_auth.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  subnet_id              = aws_subnet.my_public_subnet.id
+
+  root_block_device {
+    volume_size = 10
+  }
+
+}
