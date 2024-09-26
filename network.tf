05e73dd0b697f04a5b5b5d5a45ba4701b937f028
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2024-09-26 15:15:13 +0000

    refactor: formatted network file

--- network.tf
+++ network.tf
@@ -52,3 +52,26 @@ resource "aws_route_table_association" "public_subnet_association" {
   subnet_id      = aws_subnet.my_public_subnet.id
 
 }
+
+resource "aws_security_group" "my_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
