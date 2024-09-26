de79c69f1726685a09eb133782e08088829feef1
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2024-09-26 14:28:53 +0000

    update: updated network infrastructure

--- network.tf
+++ network.tf
@@ -1,10 +1,54 @@
 resource "aws_vpc" "my_vpc" {
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
 
-    
-    tags ={
-        Name: "dev"
-    }
-  
+
+  tags = {
+    Name : "dev"
+  }
+
+}
+
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev_public_subnet"
+  }
+}
+
+resource "aws_internet_gateway" "my_IGW" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "dev_IGW"
+  }
+
+}
+
+
+resource "aws_route_table" "my_public_route_TB" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "dev_public_route_table"
+  }
+
+}
+
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.my_public_route_TB.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_IGW.id
+
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  route_table_id = aws_route_table.my_public_route_TB.id
+  subnet_id      = aws_subnet.my_public_subnet.id
+
 }
