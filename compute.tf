6e55a9a06f0b79b7f9998ee813e9c0e86c99852d
Sarfo-Anokye <isaac.sarfo-anokye@amalitech.org>
2024-09-27 11:52:43 +0000

    update: updated compute resources

--- compute.tf
+++ compute.tf
@@ -5,20 +5,21 @@ resource "aws_key_pair" "ec2_auth" {
 }
 
 resource "aws_instance" "dev_node" {
-  instance_type = "t2.micro"
-  ami           = data.aws_ami.public_ec2_ami.id
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.public_ec2_ami.id
+  key_name               = aws_key_pair.ec2_auth.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  subnet_id              = aws_subnet.my_public_subnet.id
+  user_data              = file("./userdata.tpl")
 
+
+  root_block_device {
+    volume_size = 10
+  }
   tags = {
     Name = "dev_node"
 
   }
 
-  key_name               = aws_key_pair.ec2_auth.id
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-  subnet_id              = aws_subnet.my_public_subnet.id
-
-  root_block_device {
-    volume_size = 10
-  }
 
 }
